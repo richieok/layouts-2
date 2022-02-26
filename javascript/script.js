dfcf1eea3c9efb87d1601287905c7b9e8cc5d155
richieok <richie.okoro@gmail.com>
2022-02-26 07:26:47 +0100

    js populates the frame

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,29 @@
 window.onload = function(){
     console.log('Loaded');
 };
+
+const frame = document.getElementById('frame');
+
+const game = document.createElement('div');
+//outer grid
+game.classList.add('grid');
+game.classList.add('major');
+
+// let gridArray = [];
+
+for (let i = 0; i < 9; i++){
+    //inner gird
+    let innerGrid = document.createElement('div');
+    innerGrid.classList.add('grid');
+    innerGrid.classList.add('minor');
+    for (let n = 0; n < 9; n++){
+        let card = document.createElement('div')
+        card.classList.add('card');
+        let text = document.createTextNode('');
+        card.appendChild(text);
+        innerGrid.appendChild(card);
+    }
+    game.appendChild(innerGrid);
+}
+
+frame.appendChild(game);
