f0314023870001889b84208c788a1d14ce8a9e9c
techrifique <techrifique@gmail.com>
2022-02-28 13:19:51 +0100

    the sudoku data arrangement is modified by a for loop into a 2D array

--- javascript/gamedata.js
+++ javascript/gamedata.js
@@ -1,7 +1,12 @@
 export function genData(){
     let a = []
     for (let i = 0; i < 81; i ++){
+        // a.push(Math.floor(Math.random()*10));
         a.push(i);
     }
-    return a;
+    return {
+        rows: 9,
+        cols: 9,
+        data: a
+    };
 }
--- javascript/script.js
+++ javascript/script.js
@@ -34,7 +34,37 @@ frame.appendChild(game);
 
 const cards = document.getElementsByClassName('card');
 for ( let i = 0; i < cards.length; i++ ){
-    cards[i].childNodes[0].data = a[i]
+    cards[i].childNodes[0].data = a.data[i]
 }
-console.dir(cards[0]);
-// cards[0].childNodes[0].data = '9'
+
+// const rows = a.data.length/9;
+let organised = [];
+// console.log(organised)
+
+for ( let i = 0; i < a.rows; i++){
+    // console.log(a.data.slice(i*9, (i*9)+9));
+    // organised[i] = a.data.slice(i * 9, (i * 9) + 9);
+    // console.dir(a.data.slice(i * a.cols, i * a.cols + a.cols ));
+    organised[i] = a.data.slice(i * a.cols, (i * a.cols) + a.cols);
+    // console.dir(organised);
+}
+
+// console.dir(organised);
+organised = [];
+
+//convert the incoming array into a 2d array
+for (let i = 0; i < 3; i++){
+    console.log(i);
+    let start = i * (3 * 9);
+    console.log(`start = ${start}`);
+    organised[(i * 3)] = [].concat(a.data.slice(start, start + 3), a.data.slice(start + 9, start + 9 + 3), a.data.slice(start + 18, start + 18 + 3));
+    start = start + 3;
+    console.log(`next = ${start}`);
+    organised[(i * 3) + 1] = [].concat(a.data.slice(start, start + 3), a.data.slice(start + 9, start + 9 + 3), a.data.slice(start + 18, start + 18 + 3));
+    
+    start = start + 3;
+    console.log(`next = ${start}`);
+    organised[(i * 3) + 2] = [].concat(a.data.slice(start, start + 3), a.data.slice(start + 9, start + 9 + 3), a.data.slice(start + 18, start + 18 + 3));
+    
+}
+console.dir(organised);
